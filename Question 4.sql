aed9436999d0a26ce58272ec6a87259ec2ef98de
Guilherme Sampaio <gui85238523@gmail.com>
2021-03-04 11:11:43 -0300

    Delete Question 4.sql
    Remoção de arquivo

--- Question 4.sql
+++
@@ -1,5 +0,0 @@
-SELECT order.OrderID, SUM (order.Quantity * order.OrderPrice) 
-    AS TotalPrice FROM Customer custo 
-        INNER JOIN Orders order ON order.CustomerId = custo.CustomerId 
-            AND custo.CustomerName = 'DeathStar'
-                GROUP BY order.OrderID;
